3823e83f1ba5afd0198cb5feb33b941708638ef8
zach <zclewell@gmail.com>
2020-03-01 19:16:51 -0600

    Formatting and implementing weak search

--- app.js
+++ app.js
@@ -2,31 +2,47 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 const secrets = require('./secrets');
 
-var app = express();
+const app = express();
 
-const uri = 'mongodb+srv://' + secrets.username + ':' + secrets.password + '@' + secrets.server + '/test?retryWrites=true&w=majority';
+const uri = `mongodb+srv://${secrets.username}:${secrets.password}@${secrets.server}/test?retryWrites=true&w=majority`;
 const client = MongoClient(uri);
 
+function getRoot(req, res) {
+  res.end('Hello World!');
+}
 
-app.get('/', function(req, res) {
-    res.end('Hello World!');
-});
+async function getNumber(req, res) {
+  const raw = parseInt(req.params.id, 10);
 
-app.get('/number/:id', async function(req, res) {
-    var result = await client.db('monDb').collection('monCollection').findOne({number: req.params.id});
-    
-    res.end(JSON.stringify(result));
-});
+  let formatted = raw.toString();
+  if (raw < 10) {
+    formatted = `00${formatted}`;
+  } else if (raw < 100) {
+    formatted = `0${formatted}`;
+  }
 
-app.get('/name/:id', async function(req, res) {
-    var result = await client.db('monDb').collection('monCollection').findOne({name: req.params.id});
+  const query = { number: formatted };
+  const result = await client.db('monDb').collection('monCollection').findOne(query);
 
-    res.end(JSON.stringify(result));
-})
+  res.end(JSON.stringify(result));
+}
 
-var server = app.listen(8081, async function () {
-    await client.connect();
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("Example app listening at http://%s:%s", host, port);
-});
+async function getSearch(req, res) {
+  const { term } = req.params;
+
+  let query = { name: term };
+  let result = await client.db('monDb').collection('monCollection').find(query).toArray();
+  query = { number: term };
+  result = result.concat(await client.db('monDb').collection('monCollection').find(query).toArray());
+
+  res.end(JSON.stringify(result));
+}
+
+app.get('/', getRoot);
+app.get('/number/:id', getNumber);
+app.get('/search/:term', getSearch);
+
+async function serverFunc() {
+  await client.connect();
+}
+app.listen(8081, serverFunc);
